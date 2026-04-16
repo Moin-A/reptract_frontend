89f4b30f2226c602f7552bb2566a7abdf5f1b442
Apple <apple@Apples-MacBook-Pro.local>
2026-04-16 18:21:44 +0530

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.figma.com",
-      },
-    ],
-  },
+  allowedDevOrigins: ["http://localhost:3000"],
+  /* config options here */
 };
 
 export default nextConfig;
